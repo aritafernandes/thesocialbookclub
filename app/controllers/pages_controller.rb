7af2b754f7d1f66cd2b6819b6ac483a578a50a21
rita fernandes <ritaestevesfernandes@gmail.com>
2023-06-06 15:51:40 +0100

    saving changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @bookclubs = Bookclub.all.first(3)
   end
 end
--- app/models/bookclub.rb
+++ app/models/bookclub.rb
@@ -1,4 +1,5 @@
 class Bookclub < ApplicationRecord
   has_many :meetings
   has_many :bookclub_members
+  
 end
