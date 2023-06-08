a2312f1ddb6be92f2ed61b1b52fde43151a36415
rita fernandes <ritaestevesfernandes@gmail.com>
2023-06-08 17:54:43 +0100

    users avatars and users and bookclub members seeds

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
   include Pundit::Authorization
 
@@ -10,5 +11,11 @@ class ApplicationController < ActionController::Base
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:photo, :email, :password)}
+
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:photo, :email, :password, :current_password)}
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,8 @@
 
 require "json"
 require "open-uri"
-puts "Bookclub_members"
+
+puts "Destroying all Bookclub_members"
 puts "---------------------------"
 BookclubMember.destroy_all
 puts "Destroying all meetings"
@@ -17,9 +18,13 @@ Meeting.destroy_all
 puts "Destroying all books"
 puts "---------------------------"
 Book.destroy_all
-puts "Books Destroyed"
+puts "Destroying all bookclubs"
 puts "---------------------------"
 Bookclub.destroy_all
+puts "Destroying all users"
+puts "---------------------------"
+User.destroy_all
+
 
 authors = ["toni+morrison", "annie+ernaux", "jose+saramago", "david+sedaris"]
 
@@ -44,19 +49,23 @@ authors.each do |author|
   Book.create!(title: title, author: author, description: description, image_link:image, year: year)
 
   puts "created book"
+  puts "---------------------------"
 
   end
 end
 
 puts "creating bookclub 1"
+puts "---------------------------"
 file = URI.open("https://api.bookclubs.com/files/club_images/32267/avatar.jpg.200x200_q85_crop.jpg")
 bookclub = Bookclub.new(name: "Nasty Women Book Club", description: "We are a feminist book club and read books about strong female leads. The name Nasty Women comes from the 2016 US presidential debate where Donald Trump referred to Hillary Clinton as a Nasty Woman. We love reading all books with a feminist take, and proudly claim Roxane Gay as our Patron Saint.")
 bookclub.photo.attach(io: file, filename: "#{bookclub.name}.jpg", content_type: "image/jpg")
 bookclub.save!
 
 puts "created bookclub 1"
+puts "---------------------------"
 
 puts "creating bookclub 2"
+puts "---------------------------"
 
 file = URI.open("https://api.bookclubs.com/files/club_images/5968920/87363davatar.jpg.200x200_q85_crop.png")
 bookclub = Bookclub.new(name: "The Horror Fiction Book Club", description: "The Horror Fiction Book Club (mostly) meets on the final Monday of each month (7-8 pm). Even though we are a library book club, members are not required to live in Prince William County to attend meetings. All you need to do is read the book and click the link to join in the conversation! Those brave enough can join us virtually via Webex.")
@@ -64,8 +73,10 @@ bookclub.photo.attach(io: file, filename: "#{bookclub.name}.png", content_type: 
 bookclub.save!
 
 puts "created bookclub 2"
+puts "---------------------------"
 
 puts "creating bookclub 3"
+puts "---------------------------"
 
 file = URI.open("https://api.bookclubs.com/files/jbc_form_avatars/1146/63bb9favatar.jpg.200x200_q85_crop.png")
 bookclub = Bookclub.new(name: "Manga and Books", description: "Here, we will discuss books, and manga.")
@@ -73,8 +84,10 @@ bookclub.photo.attach(io: file, filename: "#{bookclub.name}.png", content_type: 
 bookclub.save!
 
 puts "created bookclub3"
+puts "---------------------------"
 
 puts "creating bookclub 4"
+puts "---------------------------"
 
 file = URI.open("https://api.bookclubs.com/files/club_images/5977226/avatar.png.200x200_q85_crop.png")
 bookclub = Bookclub.new(name: "Queer Science Fiction and Fantasy", description: "An open social space for reading and discussing queer SFF / speculative fiction, from novels to comics to story collections! Meeting monthly at the Culver Steps.")
@@ -82,8 +95,10 @@ bookclub.photo.attach(io: file, filename: "#{bookclub.name}.png", content_type: 
 bookclub.save!
 
 puts "created bookclub 4"
+puts "---------------------------"
 
 puts "creating bookclub 5"
+puts "---------------------------"
 
 file = URI.open("https://api.bookclubs.com/files/club_images/5994749/c88bfaavatar.jpg.200x200_q85_crop.png")
 bookclub = Bookclub.new(name: "Science, Philosophy and Classical Literature", description: "About Science, Philosophy and Classical Literature")
@@ -91,3 +106,34 @@ bookclub.photo.attach(io: file, filename: "#{bookclub.name}.png", content_type: 
 bookclub.save!
 
 puts "created bookclub 5"
+puts "---------------------------"
+
+puts "create user admins"
+puts "---------------------------"
+
+user1 = User.create(email: "rita@admin.com", password: "123123")
+user2 = User.create(email: "thamara@admin.com", password: "123123")
+user3 = User.create(email: "gabriel@admin.com", password: "123123")
+
+puts "created user admins"
+puts "---------------------------"
+
+puts "create bookclub members"
+puts "---------------------------"
+
+bookclubmember1 = BookclubMember.create(user: user1, bookclub:bookclub, admin: true)
+bookclubmember2 = BookclubMember.create(user: user2, bookclub:bookclub )
+bookclubmember3 = BookclubMember.create(user: user3, bookclub:bookclub)
+
+puts "created bookclub members"
+puts "---------------------------"
+
+puts "create user1 photo"
+puts "---------------------------"
+
+file = URI.open("https://images.gr-assets.com/users/1641123162p8/145281070.jpg")
+user1.photo.attach(io: file, filename: "#{user1.photo}.jpg", content_type: "image/jpg")
+user1.save!
+
+puts "created user1 photo"
+puts "---------------------------"
