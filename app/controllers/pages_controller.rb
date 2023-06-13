fb85e9370dfd5ec4ce95161edb45c4637d8429d8
rita fernandes <ritaestevesfernandes@gmail.com>
2023-06-13 17:27:21 +0100

    fix multiple issues including home cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[home index show]
 
   def home
-    @bookclubs = Bookclub.where.associated(:books).first(3)
+    @bookclubs = Bookclub.first(3)
     @meetings = Meeting.all.includes(:book).last(5)
   end
 end
--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -3,7 +3,7 @@ class Meeting < ApplicationRecord
   has_many :meeting_guests
   belongs_to :book
 
-  enum status: { pending: 0, accepted: 1, declined: 2 }
+  enum :status, { pending: 0, accepted: 1, declined: 2 }
 
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
